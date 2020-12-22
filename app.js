f9a6872ae1a608d314f166a57912da1eec4bf102
SSR <ssrstudio99@gmail.com>
2020-12-23 00:42:40 +0530

    01 - tourController built

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
+const tourRouter = require('./Routes/tourRoutes');
+const userRouter = require('./Routes/userRoutes');
 
 const app = express();
 
 // 1) MIDDLEWARES
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const color = require('colors');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const port = process.env.PORT || 3000;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('🦖 DB Connection SUCCESSFUL...'.cyan.bold));
+
+const port = process.env.PORT || 9000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  console.log(`🚀 App running on port ${port}...`.white.bold);
 });
